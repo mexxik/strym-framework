53ca411a899bea2e1c44fb095473ab66e1e49e20
Max Golovanchuk <mexxik@gmail.com>
2015-08-28 17:32:23 +0300

    exposing connectors

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var util        = require('util');
 var Container   = require('./lib/container');
 var Service     = require('./lib/basic/service');
 
+var InternalConnector   = require('./lib/connectors/internal');
+var AMQPConnector       = require('./lib/connectors/amqp');
+var SocketConnector     = require('./lib/connectors/socket');
+
 module.exports = {
     createContainer: function(configPath) {
         var container = new Container(configPath);
@@ -10,5 +14,14 @@ module.exports = {
     },
     inheritService: function(childService) {
         util.inherits(childService, Service);
+    },
+    InternalConnector: function() {
+        return InternalConnector;
+    },
+    AMQPConnector: function() {
+        return AMQPConnector;
+    },
+    SocketConnector: function() {
+        return SocketConnector;
     }
 };
